de03e2b181038d82f767283c16cd9bb6f315127a
yousero <youser8@gmail.com>
2025-02-01 21:26:28 +0500

    add functions

--- main.js
+++ main.js
@@ -2,22 +2,40 @@
 /**
  * snake <https://yousero.github.io/snake/>
  * @author yousero yousero.art@gmail.com
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 const canvas = document.getElementById('canvas')
+const source = document.createElement('canvas')
 
-canvas.height = 480
-canvas.width = 480
+canvas.height = source.height = 480
+canvas.width = source.width = 480
 
-const ctx = canvas.getContext('2d')
+const ctx = source.getContext('2d')
 
 const background = {
-  color: '#f1f1f1',
+  color: '#030403',
   draw() {
     ctx.fillStyle = this.color
     ctx.fillRect(0, 0, canvas.width, canvas.height)
   }
 }
 
-background.draw()
+const update = () => {
+  const destCtx = canvas.getContext('2d')
+  destCtx.drawImage(source, 0, 0)
+}
+
+const draw = () => {
+  background.draw()
+}
+
+const run = () => {
+  draw()
+  update()
+
+  requestAnimationFrame(run)
+}
+
+draw()
+update()
