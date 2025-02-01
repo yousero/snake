8b835f4e96ab94f61aab36b96d256cae987ca13c
yousero <youser8@gmail.com>
2025-02-01 23:43:07 +0500

    add basic.css, gridBorder

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /**
  * snake <https://yousero.github.io/snake/>
  * @author yousero yousero.art@gmail.com
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 const canvas = document.getElementById('canvas')
@@ -10,6 +10,10 @@ const source = document.createElement('canvas')
 
 canvas.height = source.height = 480
 canvas.width = source.width = 480
+
+let border = 2
+let size = 12
+let margin = 2
 
 const ctx = source.getContext('2d')
 
@@ -21,6 +25,14 @@ const background = {
   }
 }
 
+const gridBorder = {
+  color: '#04f404',
+  draw() {
+    ctx.strokeStyle = this.color
+    ctx.strokeRect(0 + 0.5, 0 + 0.5, canvas.width - 1, canvas.height - 1)
+  }
+}
+
 const update = () => {
   const destCtx = canvas.getContext('2d')
   destCtx.drawImage(source, 0, 0)
@@ -28,6 +40,7 @@ const update = () => {
 
 const draw = () => {
   background.draw()
+  gridBorder.draw()
 }
 
 const run = () => {
